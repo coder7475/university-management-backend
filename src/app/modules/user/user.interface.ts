a6c1af07bf532bd88c6d6ca1c46ce28f10533401
coder7475 <robiulhossain7475@gmail.com>
2024-05-29 20:06:04 +0600

    feat: create user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,10 @@
+interface IUser {
+    id: string;
+    password: string;
+    needPasswordChange: boolean;
+    role: 'admin' | 'student' | 'faculty';
+    status: 'in-progress' | 'blocked';
+    isDeleted: boolean;
+}
+
+export default IUser;
